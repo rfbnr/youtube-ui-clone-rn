d7c78ba6b30bb2e10b0ace1b9ab5140ed5b8b03c
ridwanfbnr <ridwanfebnur9@gmail.com>
2023-04-20 05:51:05 +0700

    initial commit

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "./screens/HomeScreen";
+
+export default function AppNavigation() {
+  const Stack = createNativeStackNavigator();
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          options={{ headerShown: false }}
+          component={HomeScreen}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ screens/HomeScreen.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <View className="flex-1 bg-white justify-center items-center">
+      <Text>HomeScreen</Text>
+    </View>
+  );
+}
